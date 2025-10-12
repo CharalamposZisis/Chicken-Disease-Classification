4fb6b6ca1af5486a57a315c6a51a93e61876440b
Zisis Charalampos <xarisban@gmail.com>
2025-10-12 11:38:40 +0300

    change to app.py

--- app.py
+++ app.py
@@ -11,33 +11,31 @@ app = Flask(__name__)
 CORS(app)
 
 class ClientApp:
-    def __init__(self, app):
+    def __init__(self):
         self.filename = 'inputImage.jpg'
         self.classifier = PredictionPipeline(self.filename)
-        
-        
-@app.route("/", methods=['GET'])  # route to display the home page
+
+# Initialize once
+clApp = ClientApp()
+
+@app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template("index.html")
 
-
-@app.route("/train", methods=['GET', 'POST'])  # route to show the predictions in a web UI
+@app.route("/train", methods=['GET', 'POST'])
 @cross_origin()
 def trainRoute():
-    os.system('python main.py')
+    os.system('python main.py')  # consider background task
     return "Training Completed!"
 
-
-@app.route("/predict", methods=['POST'])  # route to show the predictions in a web UI
+@app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
     image = request.json['image']
-    decodeImage(image, ClApp.filename)
-    result = ClApp(app).classifier.predict()
+    decodeImage(image, clApp.filename)
+    result = clApp.classifier.predict()
     return jsonify(result)
 
-
 if __name__ == "__main__":
-    clApp = ClientApp(app)
     app.run(host='0.0.0.0', port=8050)
